775e84bfbbebeb8408c511a65db7eed5358d39be
Gabriel Ferreira <gabrielfsilva936@gmail.com>
2025-03-13 18:44:10 -0300

    feat: adicionando atributo de saldo ao DTO de cadastro de usuário

--- src/main/java/com/dev/gabriel/desafio_picpay_simplificado/application/dto/CadastrarUsuarioDTO.java
+++ src/main/java/com/dev/gabriel/desafio_picpay_simplificado/application/dto/CadastrarUsuarioDTO.java
@@ -1,4 +1,11 @@
 package com.dev.gabriel.desafio_picpay_simplificado.application.dto;
 
+import java.math.BigDecimal;
+
 public record CadastrarUsuarioDTO(
-    String nomeCompleto, String cpfCnpj, String email, String senha, String tipo) {}
+    String nomeCompleto,
+    String cpfCnpj,
+    String email,
+    String senha,
+    String tipo,
+    BigDecimal saldoInicial) {}
