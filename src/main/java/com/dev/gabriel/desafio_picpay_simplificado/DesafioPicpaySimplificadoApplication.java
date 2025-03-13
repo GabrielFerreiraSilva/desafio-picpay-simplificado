20e40d20a6fdbe44addda3688379759d4f1f33bb
Gabriel Ferreira <gabrielfsilva936@gmail.com>
2025-03-13 18:02:26 -0300

    build: configuração inicial do projeto

---
+++ src/main/java/com/dev/gabriel/desafio_picpay_simplificado/DesafioPicpaySimplificadoApplication.java
@@ -0,0 +1,12 @@
+package com.dev.gabriel.desafio_picpay_simplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPicpaySimplificadoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DesafioPicpaySimplificadoApplication.class, args);
+  }
+}
---
+++ src/test/java/com/dev/gabriel/desafio_picpay_simplificado/DesafioPicpaySimplificadoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.dev.gabriel.desafio_picpay_simplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPicpaySimplificadoApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
