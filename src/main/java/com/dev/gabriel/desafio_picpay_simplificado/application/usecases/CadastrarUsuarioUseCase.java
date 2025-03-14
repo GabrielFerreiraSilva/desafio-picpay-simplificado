9785fa2bb1924af1011b428c598ccd6bce99fffd
Gabriel Ferreira <gabrielfsilva936@gmail.com>
2025-03-14 20:16:08 -0300

    refactor: refatorando o caso de uso para lançar as exceções customizadas

--- src/main/java/com/dev/gabriel/desafio_picpay_simplificado/application/usecases/CadastrarUsuarioUseCase.java
+++ src/main/java/com/dev/gabriel/desafio_picpay_simplificado/application/usecases/CadastrarUsuarioUseCase.java
@@ -3,6 +3,8 @@ package com.dev.gabriel.desafio_picpay_simplificado.application.usecases;
 import com.dev.gabriel.desafio_picpay_simplificado.adapters.repository.UsuarioRepository;
 import com.dev.gabriel.desafio_picpay_simplificado.application.dto.CadastrarUsuarioDTO;
 import com.dev.gabriel.desafio_picpay_simplificado.domain.enums.TipoUsuario;
+import com.dev.gabriel.desafio_picpay_simplificado.domain.exception.DadoInvalidoException;
+import com.dev.gabriel.desafio_picpay_simplificado.domain.exception.DadoJaCadastradoException;
 import com.dev.gabriel.desafio_picpay_simplificado.domain.model.Usuario;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,22 +20,24 @@ public class CadastrarUsuarioUseCase {
   @Transactional
   public void cadastrarUsuario(CadastrarUsuarioDTO dto) {
     if (this.usuarioRepository.existsByCpfCnpj(dto.cpfCnpj())) {
-      throw new RuntimeException("CPF já cadastrado no sistema");
+      throw new DadoJaCadastradoException(
+          "O CPF/CNPJ " + dto.cpfCnpj() + " já está cadastrado no sistema");
     }
 
     if (this.usuarioRepository.existsByEmail(dto.email())) {
-      throw new RuntimeException("Email já cadastrado no sistema");
+      throw new DadoJaCadastradoException(
+          "O Email " + dto.email() + " já está cadastrado no sistema");
     }
 
     if (dto.saldoInicial().compareTo(BigDecimal.ZERO) < 0) {
-      throw new RuntimeException("O saldo inicial não pode ser negativo");
+      throw new DadoInvalidoException("O saldo inicial não pode ser negativo");
     }
 
     TipoUsuario tipo;
     try {
       tipo = TipoUsuario.valueOf(dto.tipo());
     } catch (IllegalArgumentException ex) {
-      throw new RuntimeException("Tipo de usuário inválido");
+      throw new DadoInvalidoException("Tipo de usuário " + dto.tipo() + " inválido");
     }
 
     Usuario novoUsuario =
