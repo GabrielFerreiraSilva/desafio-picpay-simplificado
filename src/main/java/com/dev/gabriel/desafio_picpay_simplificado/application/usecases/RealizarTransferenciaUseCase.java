9fe156955617cbea203098410e3b07ffba073fe8
Gabriel Ferreira <gabrielfsilva936@gmail.com>
2025-03-14 20:29:11 -0300

    refactor: refatorando o caso de uso para lançar as exceções customizadas

--- src/main/java/com/dev/gabriel/desafio_picpay_simplificado/application/usecases/RealizarTransferenciaUseCase.java
+++ src/main/java/com/dev/gabriel/desafio_picpay_simplificado/application/usecases/RealizarTransferenciaUseCase.java
@@ -7,6 +7,8 @@ import com.dev.gabriel.desafio_picpay_simplificado.application.service.Consultar
 import com.dev.gabriel.desafio_picpay_simplificado.application.service.NotificarRecebimentoService;
 import com.dev.gabriel.desafio_picpay_simplificado.domain.enums.StatusTransferencia;
 import com.dev.gabriel.desafio_picpay_simplificado.domain.enums.TipoUsuario;
+import com.dev.gabriel.desafio_picpay_simplificado.domain.exception.OperacaoNaoPermitida;
+import com.dev.gabriel.desafio_picpay_simplificado.domain.exception.RecursoNaoEncontradoException;
 import com.dev.gabriel.desafio_picpay_simplificado.domain.model.Transferencia;
 import com.dev.gabriel.desafio_picpay_simplificado.domain.model.Usuario;
 import lombok.RequiredArgsConstructor;
@@ -28,23 +30,22 @@ public class RealizarTransferenciaUseCase {
     Usuario payer = fetchUsuario(dto.payer());
 
     if (payer.getTipo() == TipoUsuario.LOJISTA) {
-      throw new RuntimeException("Usuários lojistas não podem realizar transferências");
+      throw new OperacaoNaoPermitida("Usuários lojistas não podem realizar transferências");
     }
 
     if (payer.getSaldo().compareTo(dto.valor()) < 0) {
-      throw new RuntimeException("Saldo insuficiente");
+      throw new OperacaoNaoPermitida("Saldo insuficiente");
     }
 
-    Usuario payee = fetchUsuario(dto.payee());
     boolean autorizado = this.consultarAutorizadorService.ConsultarServicoAutorizador(dto);
     StatusTransferencia status =
         autorizado ? StatusTransferencia.AUTORIZADA : StatusTransferencia.NAO_AUTORIZADA;
 
-    Transferencia transferencia = new Transferencia(null, dto.valor(), payer, payee, null, status);
-
-    this.transferenciaRepository.save(transferencia);
-
     if (autorizado) {
+      Usuario payee = fetchUsuario(dto.payee());
+      Transferencia transferencia =
+          new Transferencia(null, dto.valor(), payer, payee, null, status);
+      this.transferenciaRepository.save(transferencia);
       payer.setSaldo(payer.getSaldo().subtract(dto.valor()));
       payee.setSaldo(payee.getSaldo().add(dto.valor()));
       this.usuarioRepository.saveAll(List.of(payer, payee));
@@ -57,6 +58,9 @@ public class RealizarTransferenciaUseCase {
   private Usuario fetchUsuario(Long id) {
     return this.usuarioRepository
         .findById(id)
-        .orElseThrow(() -> new RuntimeException("Usuário com o ID " + id + " não foi encontrado"));
+        .orElseThrow(
+            () ->
+                new RecursoNaoEncontradoException(
+                    "Usuário com o ID " + id + " não foi encontrado"));
   }
 }
